c04898d94de9633e1fbae2f69465087e791c01a5
leandrotarefas2 <leandro.tarefas@gmail.com>
2025-04-01 09:41:00 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const { v4: uuidv4 } = require('uuid');
 
 const express = require('express');
 const app = express();
-const port = 8080;
+const port = 3000;
 
 // Define EJS como engine de visualização
 app.set('view engine', 'ejs');
@@ -10,10 +10,19 @@ app.set('view engine', 'ejs');
 // Arquivos estáticos (CSS, imagens etc.)
 app.use(express.static('public'));
 
+app.use(express.json()); // para JSON
+app.use(express.urlencoded({ extended: true })); // para formulários URL-encoded
+
 // Rota principal
 app.get('/', (req, res) => {
     const deviceId = "9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d"+uuidv4();
-    res.render('index', { deviceId });
+    res.render('index', { deviceId, country:"AR" });
+});
+
+app.post('/', (req, res) => {
+    const { country } = req.body;
+    const deviceId = "9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d"+uuidv4();
+    res.render('index', { deviceId, country });
 });
 
 app.listen(port, () => {
